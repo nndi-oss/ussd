2dc3dd8feff349e8ee45f2b6f92b66cb25d569e7
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2020-04-24 01:32:23 +0200

    Add menu entry and concept handlers for services
    Added a menu item to enable admins to manage services
    via the USSD app.

--- go.mod
+++ go.mod
@@ -1,13 +1,13 @@
 module github.com/nndi-oss/ussd
 
 require (
-	bitbucket.org/nndi/phada v0.0.0-20180621152419-e0115143476e
+	bitbucket.org/nndi/phada v0.0.0-20200108220958-c88f9f9f8c56
 	github.com/arduino/go-system-stats v0.0.0-20180215112344-1598cba505aa
 	github.com/arduino/gonetworkmanager v0.0.0-20180215112023-793c32a95fe2
 	github.com/cloudfoundry/gosigar v1.1.0
 	github.com/dustin/go-humanize v1.0.0
 	github.com/godbus/dbus v4.1.0+incompatible
-	github.com/orcaman/concurrent-map v0.0.0-20180319144342-a05df785d2dc
+	github.com/orcaman/concurrent-map v0.0.0-20190107190726-7ed82d9cb717
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/satori/go.uuid v1.2.0
 	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
--- main.go
+++ main.go
@@ -11,21 +11,49 @@ import (
 
 const (
 	STATE_MENU          = 0
+	STATE_PROCESS_MENU  = 1010
 	STATE_DISK_SPACE    = 1
 	STATE_MEMORY        = 2
 	STATE_NETWORK       = 3
 	STATE_TOP_PROCESSES = 4
-	STATE_NOOP          = 5
+	STATE_SERVICES_MENU = 5
+
+	STATE_PROMPT_CHECK_SERVICE   = 51
+	STATE_PROMPT_START_SERVICE   = 52
+	STATE_PROMPT_STOP_SERVICE    = 53
+	STATE_PROMPT_RESTART_SERVICE = 54
+	STATE_PROMPT_ENABLE_SERVICE  = 55
+	STATE_PROMPT_DISABLE_SERVICE = 56
+
+	STATE_CHECK_SERVICE   = 511
+	STATE_START_SERVICE   = 521
+	STATE_STOP_SERVICE    = 531
+	STATE_RESTART_SERVICE = 541
+	STATE_ENABLE_SERVICE  = 551
+	STATE_DISABLE_SERVICE = 561
+
+	STATE_EXIT = -1
 
 	USSD_MENU = `USSD Server Stats Daemon
 Host: %s
 IP: %s
 
-1. Disk Space
-2. Memory
-3. Network
-4. Top Processes
-#. Quit`
+1 Disk Space
+2 Memory
+3 Network
+4 Top Processes
+5 Services`
+
+	SERVICES_MENU = `Services
+
+1 Check Status
+2 Start Service
+3 Stop Service
+4 Restart Service
+5 Enable Service
+6 Disable Service
+
+* Main Menu`
 )
 
 var (
@@ -42,7 +70,7 @@ type UssdApp struct {
 func newUssdApp(sessionStore phada.SessionStore) *UssdApp {
 	return &UssdApp{
 		sessionStore: sessionStore,
-		CurrentState: STATE_NOOP,
+		CurrentState: STATE_EXIT,
 	}
 }
 
@@ -54,50 +82,64 @@ func ussdEnd(text string) string {
 	return fmt.Sprintf("END %s", text)
 }
 
+func (u *UssdApp) SaveSession(session *phada.UssdRequestSession, nextState int) {
+	session.SetState(nextState)
+	u.sessionStore.PutHop(session)
+}
+
 func (u *UssdApp) handler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 
-	session, err := phada.ParseUssdRequest(req)
+	newSession, err := phada.ParseUssdRequest(req)
 	if err != nil {
 		log.Printf("Failed to parse UssdRequest from http.Request. Error %s", err)
 		fmt.Fprintf(w, ussdEnd("Failed to process request"))
 		return
 	}
-	session.SetState(STATE_NOOP)
+	newSession.SetState(STATE_MENU)
+	u.sessionStore.PutHop(newSession)
 
-	u.sessionStore.PutHop(session)
-
-	session, err = u.sessionStore.Get(session.SessionID)
+	session, err := u.sessionStore.Get(newSession.SessionID)
 	if err != nil {
 		log.Printf("Failed to read session %s", err)
-		fmt.Fprintf(w, ussdEnd("Failed to process request"))
-		return
+		session = newSession
 	}
 
 	if session.ReadIn() == "" {
 		session.SetState(STATE_MENU)
 	}
-	if session.ReadIn() == "1" {
-		session.SetState(STATE_DISK_SPACE)
-	}
 
-	if session.ReadIn() == "2" {
-		session.SetState(STATE_MEMORY)
-	}
+	if session.State == STATE_PROCESS_MENU {
+		if session.ReadIn() == "1" {
+			u.SaveSession(session, STATE_DISK_SPACE)
+		}
 
-	if session.ReadIn() == "3" {
-		session.SetState(STATE_NETWORK)
-	}
+		if session.ReadIn() == "2" {
+			u.SaveSession(session, STATE_MEMORY)
+		}
 
-	if session.ReadIn() == "4" {
-		session.SetState(STATE_TOP_PROCESSES)
+		if session.ReadIn() == "3" {
+			u.SaveSession(session, STATE_NETWORK)
+		}
+
+		if session.ReadIn() == "4" {
+			u.SaveSession(session, STATE_TOP_PROCESSES)
+		}
+
+		if session.ReadIn() == "5" {
+			u.SaveSession(session, STATE_SERVICES_MENU)
+			fmt.Fprintf(w, ussdContinue(SERVICES_MENU))
+			return
+		}
 	}
 
 	switch session.State {
 	case STATE_MENU:
 		text := fmt.Sprintf(USSD_MENU, hostName, bindAddress)
+		u.SaveSession(session, STATE_PROCESS_MENU)
 		fmt.Fprintf(w, ussdContinue(text))
 		break
+
 	case STATE_DISK_SPACE:
 		if isDummyServer {
 			fmt.Fprintf(w, ussdEnd(SAMPLE_DISK_STATS))
@@ -129,21 +171,96 @@ func (u *UssdApp) handler(w http.ResponseWriter, req *http.Request) {
 			fmt.Fprintf(w, ussdEnd(SAMPLE_PROC_STATS))
 		}
 		break
-	case STATE_NOOP:
+	case STATE_SERVICES_MENU:
+		serviceMenuItem := session.ReadIn()
+
+		if serviceMenuItem == "1" {
+			u.SaveSession(session, STATE_CHECK_SERVICE)
+			fmt.Fprintf(w, ussdContinue("Check Service\nEnter service name: "))
+			break
+		}
+
+		if serviceMenuItem == "2" {
+			u.SaveSession(session, STATE_START_SERVICE)
+			fmt.Fprintf(w, ussdContinue("Start Service\nEnter service name: "))
+			break
+		}
+
+		if serviceMenuItem == "3" {
+			u.SaveSession(session, STATE_STOP_SERVICE)
+			fmt.Fprintf(w, ussdContinue("Stop Service\nEnter service name: "))
+			break
+		}
+
+		if serviceMenuItem == "4" {
+			u.SaveSession(session, STATE_RESTART_SERVICE)
+			fmt.Fprintf(w, ussdContinue("Restart Service\nEnter service name: "))
+			break
+		}
+
+		if serviceMenuItem == "5" {
+			u.SaveSession(session, STATE_ENABLE_SERVICE)
+			fmt.Fprintf(w, ussdContinue("Enable Service\nEnter service name: "))
+			break
+		}
+
+		if serviceMenuItem == "6" {
+			u.SaveSession(session, STATE_DISABLE_SERVICE)
+			fmt.Fprintf(w, ussdContinue("Disable Service\nEnter service name: "))
+			break
+		}
+
+		u.SaveSession(session, STATE_EXIT)
+		break
+
+	case STATE_CHECK_SERVICE:
+		serviceName := session.ReadIn()
+		// TODO: Actually run the systemctl command here
+		fmt.Fprintf(w, ussdEnd("$ systemctl status %s\nResult: Service is active"), serviceName)
+		break
+	case STATE_START_SERVICE:
+		serviceName := session.ReadIn()
+		// TODO: Actually run the systemctl command here
+		fmt.Fprintf(w, ussdEnd("$ systemctl start %s\nResult: Service started successfully."), serviceName)
+		break
+	case STATE_STOP_SERVICE:
+		serviceName := session.ReadIn()
+		// TODO: Actually run the systemctl command here
+		fmt.Fprintf(w, ussdEnd("$ systemctl stop %s\nResult: Service stopped successfully."), serviceName)
+		break
+	case STATE_RESTART_SERVICE:
+		serviceName := session.ReadIn()
+		// TODO: Actually run the systemctl command here
+		fmt.Fprintf(w, ussdEnd("$ systemctl restart %s\nResult: Service restarted successfully."), serviceName)
+		break
+	case STATE_ENABLE_SERVICE:
+		serviceName := session.ReadIn()
+		// TODO: Actually run the systemctl command here
+		fmt.Fprintf(w, ussdEnd("$ systemctl enable %s\nResult: Service enabled successfully."), serviceName)
+		break
+	case STATE_DISABLE_SERVICE:
+		serviceName := session.ReadIn()
+		// TODO: Actually run the systemctl command here
+		fmt.Fprintf(w, ussdEnd("$ systemctl disable %s\nResult: Service disabled successfully."), serviceName)
+		break
+
+	case STATE_EXIT:
 	default:
+		u.SaveSession(session, STATE_EXIT)
 		fmt.Fprintf(w, ussdEnd("# exit()"))
 		break
 	}
+	u.sessionStore.PutHop(session)
 }
 
 func init() {
 	flag.BoolVar(&isDummyServer, "dummy", false, "Start the dummy server - uses hardcoded values")
-	flag.StringVar(&hostName, "h", "example.com", "Hostname")
+	flag.StringVar(&hostName, "hostname", "example.com", "Hostname")
 	flag.StringVar(&bindAddress, "b", "127.0.0.1:8000", "Bind address")
 }
 
 func main() {
-	ussdApp := newUssdApp(phada.NewInMemorySessionStore())
+	ussdApp := newUssdApp(phada.NewRistrettoSessionStore())
 	http.HandleFunc("/", ussdApp.handler)
 	log.Fatalf("Failed to start server. Error %s", http.ListenAndServe(bindAddress, nil))
 }
